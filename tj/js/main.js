7769a50af04b97d4fa968bb2f12e1034c938e020
fujinamitaiju <aaachokichokiaaa@gmail.com>
2019-05-19 10:13:20 +0800

    add slick slider

---
+++ tj/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+$('.slick').slick({
+	prevArrow:'<div class="prev"><</div>',
+ 　　nextArrow:'<div class="next">></div>',
+  centerMode: true,
+  centerPadding: '0px',
+  slidesToShow: 3,
+  margin: 5
+
+	
+});
+
+});
