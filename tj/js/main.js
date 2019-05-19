e19ae4c8980649c11f9b4585e17ada83dea140b8
fujinamitaiju <aaachokichokiaaa@gmail.com>
2019-05-19 21:05:53 +0800

    add navigaiton and slick for responsive

--- tj/js/main.js
+++ tj/js/main.js
@@ -1,12 +1,33 @@
 $(document).ready(function(){
+	$('.hamburger').on('click', function(){
+    $(this).toggleClass("active");
+  	$('#navigation').fadeToggle(600);
+  	
+    $('.navigation_item').on('click', function(){
+    $('#navigation').hide();
+    $('.hamburger').removeClass('active');
+  });
+})
 
 $('.slick').slick({
 	prevArrow:'<div class="prev"><</div>',
- 　　nextArrow:'<div class="next">></div>',
-  centerMode: true,
-  centerPadding: '0px',
-  slidesToShow: 3,
-  margin: 5
+ 　 nextArrow:'<div class="next">></div>',
+ 	centerMode: true,
+	centerPadding: '0px',
+	slidesToShow: 3,
+	margin: 5,
+	speed: 2000,
+	responsive: [
+	    {	
+      breakpoint: 768,
+      settings: {
+      	accessibility: true,
+        
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 1
+      }
+    }]
 
 	
 });
